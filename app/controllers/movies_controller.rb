ab2c0ee67660bec4b47ccf3f5ac815ff1726ef07
GHauer <hauer.georg@gmail.com>
2014-05-04 11:26:32 -0500

    Movies
    Added show function and formated show tabs

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
     # Your code goes here
     # Use the information after the slash in the URL to look up the movie with the corresponding ID number
     # Store the movie in the @movie instance variable so the view can format it
-
-    # @movie = ???
+    row_id = params[:id]
+    i = Movie.find(params[:id])
+    @movie = i
   end
 end
